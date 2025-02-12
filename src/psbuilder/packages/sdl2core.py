a44336638897b09e673caf3b28323af4ff7397a5
PythonSwiftLink <pythonswiftlink@gmail.com>
2025-02-12 03:48:35 +0100

    ...

--- src/psbuilder/packages/sdl2core.py
+++ src/psbuilder/packages/sdl2core.py
@@ -4,6 +4,12 @@ from psbuilder.package import SwiftPackage
 from kivy_ios.toolchain import Recipe
 from kivy_ios.recipes import sdl2, sdl2_image, sdl2_mixer, sdl2_ttf
 
+from os.path import basename, join, exists
+import os
+import plistlib
+import shutil
+
+
 PackageDependency = SwiftTarget.PackageDependency
 
 class SDL2CoreTarget(SwiftTarget):
@@ -42,6 +48,49 @@ class SDL2Core(SwiftPackage):
         return [
             SwiftPackage.Dependency("https://github.com/KivySwiftLink/ImageCore", version=self.version)
         ]
+        
+    def process_plist(self, plist: str, header_fn: str):
+        with open(plist, "rb") as rp:
+            plist_data = plistlib.load(rp)
+        
+        for lib in plist_data["AvailableLibraries"]:
+            lib["HeadersPath"] = header_fn
+        
+        with open(plist, "wb") as fp:
+            fp.write(
+                plistlib.dumps(plist_data)
+            )
+        
+    def process_xc(self, xc: str):
+        sdl_header_fn = "sdl2"
+        sdl_headers = join(self.ctx.dist_dir, "include", "common", sdl_header_fn)
+        
+        with open(join(sdl_headers, "module.modulemap"), "w") as fp:
+            fp.write(self.module_map)
+            
+        for plat in self.xc_platforms:
+            xc_plat = join(xc, plat)
+            xc_target = join(xc_plat, sdl_header_fn)
+            os.mkdir(xc_target)
+            if os.path.exists(xc_target): continue
+            
+            shutil.copytree(
+                sdl_headers,
+                xc_target
+            )
+        
+        self.process_plist(
+            join(xc,"Info.plist"),
+            sdl_header_fn
+        )
+
+    module_map = """
+        module SDL [extern_c] {
+            umbrella header "SDL.h"
+            export *
+            link "SDL"
+        }
+        """
 
 
 
